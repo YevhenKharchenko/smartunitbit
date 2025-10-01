69d9d38c7a9a756832199565128687656b36c89c
YevhenKharchenko <evgenvalerych@gmail.com>
2025-10-01 11:24:10 +0300

    Add booking, contact, faq, features, lab, puzzles and story sections

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,5 +1,3 @@
-import sprite from '../img/sprite.svg';
-
 const faqBtns = document.querySelectorAll('.faq-top-text-btn');
 faqBtns.forEach(btn => btn.addEventListener('click', onBtnClick));
 
@@ -7,13 +5,11 @@ function onBtnClick(e) {
   const faqTop = e.currentTarget;
   const faqContainer = faqTop.closest('.faq-list-item');
   const bottomText = faqContainer.querySelector('.faq-bottom-text');
-  const iconUse = faqTop.querySelector('use');
 
   document.querySelectorAll('.faq-list-item').forEach(item => {
     if (item !== faqContainer) {
       item.classList.remove('faq-open');
       item.querySelector('.faq-bottom-text').classList.remove('is-visible');
-      item.querySelector('use').setAttribute('href', `${sprite}#icon-plus`);
     }
   });
 
@@ -21,9 +17,7 @@ function onBtnClick(e) {
 
   if (bottomText.classList.contains('is-visible')) {
     faqContainer.classList.add('faq-open');
-    iconUse.setAttribute('href', `${sprite}#icon-minus`);
   } else {
     faqContainer.classList.remove('faq-open');
-    iconUse.setAttribute('href', `${sprite}#icon-plus`);
   }
 }
